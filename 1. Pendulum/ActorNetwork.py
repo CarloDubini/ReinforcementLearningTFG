778a26445e6579b41544d4c0fd17f5893b346d42
PaPardos <papardos@ucm.es>
2024-02-19 11:19:30 +0100

    Added files to pendulum backup

---
+++ 1. Pendulum/ActorNetwork.py
@@ -0,0 +1,31 @@
+import os
+import tensorflow as tf
+from keras.api._v2.keras.layers import Dense
+import keras.api._v2.keras as keras
+
+class ActorNetwork(keras.Model):
+        def __init__(self, fc1_dims=512, fc2_dims=512, n_actions = 2, name='actor', chkpt_dir='tmp\ddpg'):
+             #Inicializo la red, sus dimensiones y posibles acciones
+             super(ActorNetwork, self).__init__()
+             self.fc1_dims = fc1_dims
+             self.fc2_dims = fc2_dims
+             self.n_actions = n_actions
+
+             self.model_name = name
+             self.checkpoint_dir = chkpt_dir
+             #Guardamos el checkpoint del modelo en la dirección propia
+             self.checkpoint_file = os.getcwd() + "\\model_weights\\" + self.model_name+ "_ddpg.h5"
+
+             self.fc1 = Dense(self.fc1_dims, activation='relu')
+             self.fc2 = Dense(self.fc2_dims, activation='relu')
+             self.mu = Dense(self.n_actions, activation='tanh')
+
+        def call(self, state):
+             prob = self.fc1(state)
+             prob = self.fc2(prob)
+
+             #Se puede multiplicar si el objetivo no es del rango de 1 a -1
+             mu = self.mu(prob)
+
+
+             return mu
---
+++ 1. Pendulum/main.py
@@ -0,0 +1,75 @@
+import gym
+import numpy as np
+import matplotlib
+from utils import plot_learning_curve
+from Actor import Agent
+
+def main():
+    # Creación del entorno
+    env = gym.make('Pendulum-v1',render_mode='human')
+    n_actions = env.action_space.shape[0]
+
+    # Creación del agente con el entorno y el número de acciones adecuados
+    agent = Agent(input_dims=env.observation_space.shape, env=env, n_actions=n_actions)
+    n_games = 15  # Número de episodios a jugar
+    max_steps= 2000
+
+    # Archivo para guardar la gráfica de rendimiento
+    figure_file = 'pendulum.png'
+
+    best_score = env.reward_range[0]  # Mejor puntuación inicializada con la peor posible
+    score_history = []  # Lista para almacenar la puntuación en cada episodio
+    load_checkpoint = False  # Bandera para cargar un punto de control previo
+
+    # Si se carga un punto de control, se inicializan las transiciones en el búfer de repetición
+    if load_checkpoint:
+        n_steps = 0
+        while n_steps <= agent.batch_size:
+            env.render()
+            observation = env.reset()
+            action = env.action_space.sample()
+            observation_, reward, done, info = env.step(action)
+            agent.remember(observation, action, reward, observation_, done)
+            n_steps += 1
+        agent.learn()
+        agent.load_models()
+        evaluate = True
+    else:
+        evaluate = False
+
+    # Ciclo principal
+    for i in range(n_games):
+        observation = env.reset()[0]  # Reiniciar el entorno para un nuevo episodio
+        done = False
+        score = 0
+        j = 0
+        while not done and j < max_steps:
+            action = agent.choose_action(observation, evaluate)  # Elegir una acción
+            observation_, reward, done, info, _ = env.step(action)  # Realizar la acción en el entorno
+            score += reward  # Actualizar la puntuación acumulada
+            agent.remember(observation, action, reward, observation_[0], done)  # Almacenar la transición
+            if not load_checkpoint:
+                agent.learn()  # Aprender de la transición
+            observation = observation_  # Actualizar el estado actual
+            j += 1
+
+        score_history.append(score)  # Almacenar la puntuación del episodio
+        avg_score = np.mean(score_history[-100:])  # Calcular la puntuación media en los últimos 100 episodios
+
+        # Actualizar la mejor puntuación si se supera
+        if avg_score > best_score:
+            best_score = avg_score
+            # Guardar los modelos del agente si no se cargó un punto de control previo
+            if not load_checkpoint:
+                 agent.save_models()
+
+        # Imprimir información sobre el episodio actual
+        print('episodio', i, 'puntuación %.1f' % score, 'puntuación media %.1f' % avg_score)
+
+    # Graficar la curva de aprendizaje si no se cargó un punto de control previo
+    if not load_checkpoint:
+        x = [i + 1 for i in range(n_games)]
+        plot_learning_curve(x, score_history, figure_file)
+
+if __name__ == "__main__":
+    main()
