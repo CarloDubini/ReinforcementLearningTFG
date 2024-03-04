948881f396e6c1734cfb1fbc40e0911a1f59a469
PaPardos <papardos@ucm.es>
2024-03-04 01:42:29 +0100

    Cambios y plots

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_learning_curve(x, scores, figure_file):
+def plot_learning_curve(x, scores, figure_file, number=100):
     running_avg = np.zeros(len(scores))
     for i in range(len(running_avg)):
-        running_avg[i] = np.mean(scores[max(0, i-200):(i+1)])
+        running_avg[i] = np.mean(scores[max(0, i-number):(i+1)])
     plt.plot(x, running_avg)
-    plt.title('Running average of previous 100 scores')
+    plt.title('Running average of previous scores')
     plt.savefig(figure_file)
 
 
